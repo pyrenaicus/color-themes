31fa2f15268d73cbe6d267d65e519900190b0d71
pauguerrero <pauguerrero@gmail.com>
2021-12-01 17:59:24 +0100

    added github corner only on desktop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import { baseColor } from "./utils/colorUtils";
 import createComplementaryTheme from "./logic/createComplementaryTheme";
@@ -6,10 +6,13 @@ import ColorTheme from "./components/ColorTheme";
 import Menu from "./components/Menu";
 import Play from "./components/Play";
 import HiddenCanvas from "./components/HiddenCanvas";
+import GithubCorner from "./components/GithubCorner";
 
 export default function App() {
   const [randomHue, setRandomHue] = useState(30);
   const [colors, setColors] = useState(createComplementaryTheme(baseColor(60)));
+  // if mobile, don't render github link
+  const [isMobile, setIsMobile] = useState(false);
   console.log("first colors:");
   console.log(colors);
 
@@ -18,6 +21,11 @@ export default function App() {
   const [imgData, setImgData] = useState("");
   // useRef hook to reference savePNG, passed as props to <Menu />
   const savePNG = useRef(null);
+
+  useEffect(() => {
+    const screenSize = window.innerWidth;
+    screenSize < 600 ? setIsMobile(true) : setIsMobile(false);
+  }, []);
 
   return (
     <>
@@ -38,6 +46,7 @@ export default function App() {
         randomHue={randomHue}
       />
       <HiddenCanvas colors={colors} savePNG={savePNG} setImgData={setImgData} />
+      {!isMobile ? <GithubCorner colors={colors} /> : null}
     </>
   );
 }
